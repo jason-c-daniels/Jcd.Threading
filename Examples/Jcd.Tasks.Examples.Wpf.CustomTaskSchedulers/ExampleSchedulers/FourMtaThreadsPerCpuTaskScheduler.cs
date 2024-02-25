7adb04aa50a0753d7ae4c8badaa380ada0316360
Jason C. Daniels <jason.c.daniels@gmail.com>
2024-02-24 22:58:12 -0600

    does this make appveyor build the branch?

--- Examples/Jcd.Tasks.Examples.Wpf.CustomTaskSchedulers/ExampleSchedulers/FourMtaThreadsPerCpuTaskScheduler.cs
+++ Examples/Jcd.Tasks.Examples.Wpf.CustomTaskSchedulers/ExampleSchedulers/FourMtaThreadsPerCpuTaskScheduler.cs
@@ -5,7 +5,7 @@ namespace Jcd.Examples.Wpf.CustomTaskSchedulers.ExampleSchedulers;
 /// <summary>
 /// A <see cref="TaskScheduler"/> that uses four MTA threads per CPU to execute
 /// <see cref="Task"/> instances. Inlining is not honored. See <see cref="SimpleThreadedTaskScheduler"/>
-/// for details.
+/// for details. 
 /// </summary>
 public class FourMtaThreadsPerCpuTaskScheduler : SimpleMtaThreadedTaskScheduler
 {
